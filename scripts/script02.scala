373687daaaa08daa7e3b77c6dc41c580ef0f964e
jvitorcardoso <aspirantevitor@gmail.com>
2024-09-14 19:05:22 +0000

    Loops e tratamento de Exceções - Script 02

---
+++ scripts/script02.scala
@@ -0,0 +1,55 @@
+object CollectionsOverview {
+
+  def main(args: Array[String]): Unit = {
+  
+    // Arrays, Listas e Maps
+
+    val fruits: List[String] = List("apple", "banana", "orange")
+
+    // Acessando elementos da lista pelo índice
+    
+    val secondFruit: String = fruits(1)
+    println(s"Segundo item na lista: $secondFruit")
+
+    val ages = Map(
+      "Alice" -> 30,
+      "Bob" -> 25,
+      "Carol" -> 35
+    )
+
+    val carolAge = ages("Carol")
+
+    // Acessando elementos do dicionário pela chave
+
+    println(s"Idade da Carol: $carolAge")
+
+    // Tratamento de Exceções
+    
+    // Método tradicional
+    try {
+    
+      val tenDividedByZero = 10 / 0
+
+    } catch {
+
+      case e: ArithmeticException => println(s"Método tradicional -> Divisão por zero: $e")
+
+    }
+
+    // Usando Try para tratamento de erros + seguro
+    import scala.util.Try
+
+    val tenDividedByZeroWithTry = Try(10 / 0)
+    println(s"Valor capturado na variável criada a partir do Try: $tenDividedByZeroWithTry")
+
+    tenDividedByZeroWithTry match {
+
+      case scala.util.Success(value) => println(s"Resultado via Try: $value")
+
+      case scala.util.Failure(exception) => println(s"Erro capturado no exception: ${exception.getMessage}")
+
+    }
+
+  }
+
+}
